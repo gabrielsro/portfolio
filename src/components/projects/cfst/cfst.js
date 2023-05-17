0b30b99f2381d08c541ddcb49bee87c7aa2f1aec
Gabriel Rodriguez <gabrielsro182@gmail.com>
2023-05-17 16:41:11 -0400

    Added links for each project

--- src/components/projects/cfst/cfst.js
+++ src/components/projects/cfst/cfst.js
@@ -1,4 +1,6 @@
 import "./cfstStyle.css";
+import GitIcon from "../icons/git2.svg";
+import LinkIcon from "../icons/link2.svg";
 
 const Cfst = {
   generateContent() {
@@ -9,6 +11,33 @@ const Cfst = {
     let contentTxt = document.createElement("p");
     contentTxt.innerText = `CarFast is a web application for connecting car sellers with car buyers. CarFast allows the user to manage a vehicle inventory through CRUD operations on different data models such as cars, makes, models, etc. It supports image storage and takes advantage of relevant APIs to gather exhaustive information on each car. CarFast uses Node.js and MongoDB to deliver a SSR fullstack solution.`;
     contentContainer.appendChild(contentTxt);
+    /**
+     * Links:
+     */
+    let contentLinks = document.createElement("div");
+    contentLinks.classList.add("projectLinksContainer");
+    //GH Link
+    let gitHubIcon = new Image();
+    gitHubIcon.src = GitIcon;
+    gitHubIcon.alt = "GitHub icon";
+    gitHubIcon.classList.add("projectGHIcon");
+    let githubLink = document.createElement("a");
+    githubLink.innerText = "asasasass";
+    //Site Link
+    let linkIcon = new Image();
+    linkIcon.src = LinkIcon;
+    linkIcon.alt = "Link icon";
+    linkIcon.classList.add("projectLinkIcon");
+    let link = document.createElement("a");
+    link.innerText = "testklnsfdnkaskldfnaklsd";
+    link.setAttribute("href", "");
+
+    //Pack it all up:
+    contentLinks.appendChild(linkIcon);
+    contentLinks.appendChild(link);
+    contentLinks.appendChild(gitHubIcon);
+    contentLinks.appendChild(githubLink);
+    contentContainer.appendChild(contentLinks);
     return contentContainer;
   },
 };
--- src/components/projects/wapp/wapp.js
+++ src/components/projects/wapp/wapp.js
@@ -1,4 +1,6 @@
 import "./wappStyle.css";
+import GitIcon from "../icons/git2.svg";
+import LinkIcon from "../icons/link2.svg";
 
 const Wapp = {
   generateContent() {
@@ -9,6 +11,34 @@ const Wapp = {
     let contentTxt = document.createElement("p");
     contentTxt.innerText = `Weather App is a classic weather application that displays several weather data for different cities around the world. It was built using pure vanilla JavaScript, which means that, excluding its APIs for data gathering, it's completely client-side rendered and has a very fast UI. Users can save a list of favorite cities for ease of access, save a preferred system of measurement and see different metrics, among other features.`;
     contentContainer.appendChild(contentTxt);
+    /**
+     * Links:
+     */
+    let contentLinks = document.createElement("div");
+    contentLinks.classList.add("projectLinksContainer");
+    //GH Link
+    let gitHubIcon = new Image();
+    gitHubIcon.src = GitIcon;
+    gitHubIcon.alt = "GitHub icon";
+    gitHubIcon.classList.add("projectGHIcon");
+    let githubLink = document.createElement("a");
+    githubLink.innerText = "github.com/gabrielsro/weather";
+    githubLink.setAttribute("href", "https://github.com/gabrielsro/weather");
+    //Site Link
+    let linkIcon = new Image();
+    linkIcon.src = LinkIcon;
+    linkIcon.alt = "Link icon";
+    linkIcon.classList.add("projectLinkIcon");
+    let link = document.createElement("a");
+    link.innerText = "gabrielsro.github.io/weather";
+    link.setAttribute("href", "https://gabrielsro.github.io/weather/");
+
+    //Pack it all up:
+    contentLinks.appendChild(linkIcon);
+    contentLinks.appendChild(link);
+    contentLinks.appendChild(gitHubIcon);
+    contentLinks.appendChild(githubLink);
+    contentContainer.appendChild(contentLinks);
     return contentContainer;
   },
 };
