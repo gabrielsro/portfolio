e3eb714798a600aeb8289f1c21567f218867584f
Gabriel Rodriguez <gabrielsro182@gmail.com>
2023-06-15 12:18:48 -0400

    Added CarFast link

--- src/components/projects/cfst/cfst.js
+++ src/components/projects/cfst/cfst.js
@@ -9,7 +9,7 @@ const Cfst = {
     contentContainer.classList.add("invisible");
     contentContainer.setAttribute("id", "CarFast");
     let contentTxt = document.createElement("p");
-    contentTxt.innerText = `CarFast is a web application for connecting car sellers with car buyers. CarFast allows the user to manage a vehicle inventory through CRUD operations on different data models such as cars, makes, models, etc. It supports image storage and takes advantage of relevant APIs to gather exhaustive information on each car. CarFast uses Node.js and MongoDB to deliver a SSR fullstack solution.`;
+    contentTxt.innerText = `CarFast is a web application for connecting car sellers with car buyers. CarFast allows the user to manage a vehicle inventory through CRUD operations on different data models such as cars, makes, models, etc. It supports image storage and optimization and takes advantage of relevant APIs to gather exhaustive information on each car. CarFast uses Node.js and MongoDB to deliver a SSR fullstack solution.`;
     contentContainer.appendChild(contentTxt);
     /**
      * Links:
@@ -30,7 +30,11 @@ const Cfst = {
     linkIcon.alt = "Link icon";
     linkIcon.classList.add("projectLinkIcon");
     let link = document.createElement("a");
-    link.innerText = "pending";
+    link.innerText = "https://carfast-production.up.railway.app/inventory";
+    link.setAttribute(
+      "href",
+      "https://carfast-production.up.railway.app/inventory"
+    );
 
     //Pack it all up:
     contentLinks.appendChild(linkIcon);
