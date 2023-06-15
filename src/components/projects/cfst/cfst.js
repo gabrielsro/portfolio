7ebbaa4802f5c6031baaaa0a127abbb0be2d065f
Gabriel Rodriguez <gabrielsro182@gmail.com>
2023-06-15 14:39:21 -0400

    Changed CarFast description

--- src/components/projects/cfst/cfst.js
+++ src/components/projects/cfst/cfst.js
@@ -9,7 +9,7 @@ const Cfst = {
     contentContainer.classList.add("invisible");
     contentContainer.setAttribute("id", "CarFast");
     let contentTxt = document.createElement("p");
-    contentTxt.innerText = `CarFast is a web application for connecting car sellers with car buyers. CarFast allows the user to manage a vehicle inventory through CRUD operations on different data models such as cars, makes, models, etc. It supports image storage and optimization and takes advantage of relevant APIs to gather exhaustive information on each car. CarFast uses Node.js and MongoDB to deliver a SSR fullstack solution.`;
+    contentTxt.innerText = `CarFast is a web application that connects car sellers with car buyers. CarFast allows the user to manage a vehicle inventory through CRUD operations on different data models such as cars, makes, models, etc. It supports image storage and optimization and takes advantage of relevant APIs to gather exhaustive information on each car. CarFast uses Node.js and MongoDB to deliver a SSR fullstack solution.`;
     contentContainer.appendChild(contentTxt);
     /**
      * Links:
